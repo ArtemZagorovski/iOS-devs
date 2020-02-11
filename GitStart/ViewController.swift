d183cf89e5611d35def09b9049d0867d93499ee6
ArtemZagorovskYy <a.zagorovski23@gmail.com>
2020-02-11 16:35:50 +0300

    Initial Commit

---
+++ GitStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitStart
+//
+//  Created by Артем  on 2/11/20.
+//  Copyright © 2020 Artem Zagorovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
