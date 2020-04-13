66a9f5bda9883331808bf06c519678dd3966f190
ArtemZagorovskYy <a.zagorovski23@gmail.com>
2020-04-13 16:00:16 +0300

    feat: add the logic of doIt button

--- YOUCAN/YOUCAN/Controllers/GoalsVC.swift
+++ YOUCAN/YOUCAN/Controllers/GoalsVC.swift
@@ -41,6 +41,7 @@ class GoalsVC: UITableViewController {
         cell.iconOfGoal.image = UIImage(named: goal.typeName)
         cell.titleOfGoal.text = goal.title
         cell.progressView.progress = goal.progress
+        cell.progressView.setProgress(goal.progress, animated: true)
         cell.percentOfGoal.text = String(goal.progress)
         
         cell.doItButton.addTarget(self, action: #selector(doItButtonTapped), for: .touchUpInside)
@@ -80,31 +81,110 @@ class GoalsVC: UITableViewController {
         
     }
     
-    //MARK: Custom func
-    
     @objc func doItButtonTapped(sender: UIButton) {
         let buttonTag = sender.tag
-        
         let goal = goals[buttonTag]
+        let progress = goal.progress
         
-        var progress = goal.progress
         
-        if goal.isTime {
-            
-            var multyplier = 1
-            
-            switch goal.countOfTimesInWeek {
-            case "Every day":
-                multyplier = 30
-            case "In one day":
-                multyplier = 15
-            case "Once a week":
-                multyplier = 4
-            default:
-                print("error in goal.isTime")
+        if progress <= 1.0 && !goal.isDone {
+            if goal.isTime {
+                let todayProgress = Float(goal.timeIn) / goal.finalTask
+                try! realm.write{
+                    goal.progress = progress + todayProgress
+                }
             }
-            goal.timeIn * goal.countOfMonth
+            
+            if goal.isTimes {
+                var timesDone: Int?
+                if timesDone == nil { timesDone = 0 }
+                
+                
+                let dateComponentsFromStart = Calendar.current.dateComponents([.day, .month, .year], from: goal.startDate)
+                let dateFromSart = Calendar.current.date(from: dateComponentsFromStart)
+                let today = Date()
+                let dateComponentsToday = Calendar.current.dateComponents([.day, .month, .year], from: today)
+                let dateToday = Calendar.current.date(from: dateComponentsToday)
+                
+                switch goal.countOfTimesInString {
+                    
+                case "Day":
+                    let isToday = Calendar.current.compare(dateFromSart!, to: dateToday!, toGranularity: .day)
+                    if isToday == .orderedSame {
+                        
+                        let todayProgress: Float = 1 / Float(goal.countOfTimesIn)
+                        try! realm.write{
+                            goal.progress = progress + Float(todayProgress)
+                            if checkIsCompleted (timesDone: timesDone!, goal: goal)  { goal.isDone = true } else { timesDone! += 1 }
+                        }
+                    } else {
+                        try! realm.write{
+                            goal.progress = 0.0
+                            goal.startDate = Date()
+                        }
+                    }
+                    
+                case "Week":
+                    var dayComponent = DateComponents()
+                    dayComponent.day = 7
+                    let nextDate = Calendar.current.date(byAdding: dayComponent, to: goal.startDate)
+                    
+                    let isInWeekFirst = Calendar.current.compare(dateFromSart!, to: dateToday!, toGranularity: .day)
+                    let isInWeekSecond = Calendar.current.compare(dateToday!, to: nextDate!, toGranularity: .day)
+                    
+                    if (isInWeekFirst == .orderedSame || isInWeekFirst == .orderedDescending) && (isInWeekSecond == .orderedSame || isInWeekSecond == .orderedAscending) {
+                        let todayProgress: Float = 1 / Float(goal.countOfTimesIn)
+                        print(todayProgress)
+                        try! realm.write{
+                            goal.progress = progress + Float(todayProgress)
+                            print(goal.progress)
+                        }
+                    } else {
+                        try! realm.write{
+                            goal.progress = 0.0
+                            goal.startDate = Date()
+                        }
+                    }
+                    
+                case "Month":
+                    let isToday = Calendar.current.compare(dateFromSart!, to: dateToday!, toGranularity: .month)
+                    if isToday == .orderedSame {
+                        
+                        let todayProgress: Float = 1 / Float(goal.countOfTimesIn)
+                        try! realm.write{
+                            goal.progress = progress + Float(todayProgress)
+                            print(goal.progress)
+                        }
+                    } else {
+                        try! realm.write{
+                            goal.progress = 0.0
+                            goal.startDate = Date()
+                        }
+                    }
+                default:
+                    print("error in times")
+                }
+            }
+        }
+        DispatchQueue.main.async {
+          self.tableView.reloadData()
         }
     }
+    
+    private func checkIsCompleted (timesDone: Int, goal: Goal) -> Bool {
+        if timesDone == goal.timesIn {
+            finishGoalAlert()
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    private func finishGoalAlert() {
+        let ac = UIAlertController(title: "You did it!", message: "You have achived your goal! Congradulations!", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
 }
+
 
---
+++ YOUCAN/YOUCAN/Models/Coordinates.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinates.swift
+//  YOUCAN
+//
+//  Created by Артем  on 4/13/20.
+//  Copyright © 2020 Artem Zagorovski. All rights reserved.
+//
+
+import Foundation
