ffe92ca71b7d994c40a55315f6f91ca9cbe6031a
ArtemZagorovskYy <a.zagorovski23@gmail.com>
2020-04-29 17:34:08 +0300

    feat: add logic for getting current location of user

--- YOUCAN/YOUCAN/Managers/LocationManager.swift
+++ YOUCAN/YOUCAN/Managers/LocationManager.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var coordinates = String()
+    
+    func getCurrentLocation() -> String {
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+        return coordinates
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        coordinates = "\(locValue.latitude),\(locValue.longitude)"
+    }
+}
