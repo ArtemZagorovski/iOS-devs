5d33f4b581fba7572ea6a93e8518e060dbd41d1a
ArtemZagorovskYy <a.zagorovski23@gmail.com>
2020-04-29 17:31:55 +0300

    feat: rendered dateFormatter to another file

--- YOUCAN/YOUCAN/Helpers/Formatter.swift
+++ YOUCAN/YOUCAN/Helpers/Formatter.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import UIKit
+
+struct Formatter {
+
+    static let dateFormatter = DateFormatter()
+
+    static func getStringFromDate(date: Date) -> String {
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        return dateFormatter.string(from: date)
+    }
+    
+    static func getDateFromString(stringDate: String) -> Date {
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        return dateFormatter.date(from: stringDate)!
+    }
+    
+    static func getdateFromStringDateNone(stringDate: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.none
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.date(from: stringDate)!
+    }
+    
+    static func getStringWithWeekDay(date: Date) -> String {
+        dateFormatter.dateFormat = "EEEE, MMMM dd"
+        return dateFormatter.string(from: date)
+    }
+    
+}
